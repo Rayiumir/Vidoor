a7df5acda1d2c7e3d3b453d7fe891caaa26f7761
Rayiumir <swordart2050@gmail.com>
2025-07-22 03:07:28 +0330

    Creat Index with ID uuidv4

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const res = require('express/lib/response');
 const app = express();
 const server = require('http').Server(app);
+const {v4:uuidv4} = require('uuid');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.redirect(`/${uuidv4()}`);
+})
+
+app.get('/:id', (req, res) => {
+    res.render('index', {indexId:req.params.id});
 })
 
 server.listen(3000);
