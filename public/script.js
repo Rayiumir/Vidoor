77f602e2d2e95e9aa5ca20b3e94a26305327bd4f
Rayiumir <swordart2050@gmail.com>
2025-07-23 12:38:42 +0330

    Get microphone and webcam information

--- public/script.js
+++ public/script.js
@@ -1,5 +1,29 @@
 const socket = io();
 socket.emit('join-room', IndexID, 10);
-socket.on('userConnected', userId => {
-    console.log("user "+userId+" connected top room: " + IndexID);
+
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
 })
+
+myPeer.on('open', id = {
+    socket.emit('join-room', IndexID, id)
+})
+
+// socket.on('userConnected', userId => {
+//     console.log("user "+userId+" connected top room: " + IndexID);
+// })
+
+const videoGrid = document.getElementById('video-group');
+const video = document.createElement('videos');
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream = {
+    video.srcObject = stream;
+    video.play();
+    videoGrid.append('video');
+})
+
+video.muted = true;
