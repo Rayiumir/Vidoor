ed3c9574dfec8e91c57fd385601cb7c381b7b087
Rayiumir <swordart2050@gmail.com>
2025-07-23 18:43:32 +0330

    Sending audio and video information and displaying members present in the chat room

--- public/script.js
+++ public/script.js
@@ -5,25 +5,60 @@ const myPeer = new Peer(undefined, {
     host: '/',
     port: '3001'
 })
-
-myPeer.on('open', id = {
-    socket.emit('join-room', IndexID, id)
+myPeer.on('open', id => {
+    socket.emit('join-room', IndexID, id);
 })
 
-// socket.on('userConnected', userId => {
-//     console.log("user "+userId+" connected top room: " + IndexID);
-// })
+socket.on('userConnected', userId => {
+    //console.log("user "+userId+" connected top room: " + IndexID);
+})
 
-const videoGrid = document.getElementById('video-group');
-const video = document.createElement('videos');
+const videoGrid = document.getElementById('videoGrids');
+const video = document.createElement('video');
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true
-}).then(stream = {
-    video.srcObject = stream;
-    video.play();
-    videoGrid.append('video');
+    audio: false
+}).then(stream => {
+    const otherPeer = new Peer(undefined, {
+        host: '/',
+        port: '3001'
+    })
+    otherPeer.on('open', id => {
+        socket.emit('join-room', IndexID, id);
+    })
+    addVideoStream(video, stream);
+
+    socket.on('userConnected', userId => {
+        //console.log("user "+userId+" connected top room: " + IndexID);
+        connectToNewUser(userId, stream);
+    })
+
+    otherPeer.on('call', call => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
 })
 
 video.muted = true;
+
+function addVideoStream(video, stream){
+    video.srcObject = stream;
+    videoGrid.append(video);
+    video.play();
+}
+
+function connectToNewUser(userId, stream){
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    })
+    otherPeer.on('call', call => {
+        call.answer(stream)
+    })
+}
