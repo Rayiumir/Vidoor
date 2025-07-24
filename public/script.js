0c4505312ad82944314dba7a9806f285692216da
Rayiumir <swordart2050@gmail.com>
2025-07-24 17:44:23 +0330

    Manage webcam and microphone and create a link to invite new users to the chatroom

--- public/script.js
+++ public/script.js
@@ -1,5 +1,7 @@
 const socket = io();
 socket.emit('join-room', IndexID, 10);
+let myVideoStream;
+const peers = {}
 
 const myPeer = new Peer(undefined, {
     host: '/',
@@ -17,6 +19,7 @@ navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
+    myVideoStream = stream;
     const otherPeer = new Peer(undefined, {
         host: '/',
         port: '3001'
@@ -47,16 +50,54 @@ navigator.mediaDevices.getUserMedia({
         })
     })
 
+    const muteButton = document.querySelector("#muteButton");
+    const stopVideo = document.querySelector("#stopVideo");
+    const inviteButton = document.querySelector("#inviteButton");
+    muteButton.addEventListener("click", () => {
+        const enabled = myVideoStream.getAudioTracks()[0].enabled;
+        if(enabled){
+            myVideoStream.getAudioTracks()[0].enabled = false;
+            html = `<i class = "fas fa-microphone-slash"></i>`;
+            muteButton.classList.add("back_red");
+            muteButton.innerHTML = html;
+            
+        }else{
+            myVideoStream.getAudioTracks()[0].enabled = true;
+            html = `<i class = "fa-duotone fa-microphone"></i>`;
+            muteButton.classList.remove("backRed");
+            muteButton.innerHTML = html;
+        }
+    })
+
+    stopVideo.addEventListener("click", () => {
+        const enabled = myVideoStream.getVideoTracks()[0].enabled;
+        if(enabled){
+            myVideoStream.getVideoTracks()[0].enabled = false;
+            html = `<i class = "fa-duotone fa-video-slash"></i>`;
+            stopVideo.classList.add("backRed");
+            stopVideo.innerHTML = html;
+        }else{
+            myVideoStream.getVideoTracks()[0].enabled = true;
+            html = `<i class = "fa-duotone fa-video"></i>`;
+            stopVideo.classList.remove("backRed");
+            stopVideo.innerHTML = html;
+        }
+    })
+
+    inviteButton.addEventListener("click", () => {
+        prompt("To invite others to video chatroom, copy the following address",
+            window.location.href
+        )
+    })
+
 })
 
 function addVideoStream(video, stream){
     video.srcObject = stream;
     video.muted = true;
-    videoGrid.append(video);
     video.play();
+    videoGrid.append(video);
 }
-
-const peers = {};
 
 function connectToNewUser(userId, stream){
     const call = myPeer.call(userId, stream);
