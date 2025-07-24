a0ef225b185f6d115109917d3c6521c13aeec186
Rayiumir <swordart2050@gmail.com>
2025-07-24 16:56:23 +0330

    Detecting member exit from chat room

--- public/script.js
+++ public/script.js
@@ -14,11 +14,17 @@ const video = document.createElement('video');
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: false
+    audio: true
 }).then(stream => {
     const otherPeer = new Peer(undefined, {
         host: '/',
         port: '3001'
+    })
+    socket.on('userDisconnected', userId => {
+        console.log(userId);
+        if (peers[userId]){
+            peers[userId].close();
+        } 
     })
     otherPeer.on('open', id => {
         socket.emit('join-room', IndexID, id);
@@ -55,6 +61,11 @@ function connectToNewUser(userId, stream){
     const video = document.createElement('video');
     call.on('stream', userVideoStream => {
         addVideoStream(video, userVideoStream);
+    }) 
+
+    call.on('close', () =>{
+        video.remove();
     })
     
+    peers[userId] = call;
 }
